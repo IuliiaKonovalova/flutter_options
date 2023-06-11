811fcb590de117b9d2fb2ea5ed230a9b37420efe
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-06-11 19:20:21 +0700

    Add: set up time on load

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,10 +12,34 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  String time = 'loading';
+  Future<void> setUpWorldTime() async {
+    WorldTime instance = WorldTime(
+      location: 'Berlin',
+      flag: 'germany.png',
+      urlTail: 'Europe/Berlin',
+      time: '',
+    );
+    await instance.getTimeData();
+    setState(() {
+      time = instance.time;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+    setUpWorldTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading screen'),
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
