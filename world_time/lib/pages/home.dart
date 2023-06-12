f54eb82281fc026a9b6d7322b589adc74e96028d
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-06-12 11:32:26 +0700

    Fix: flag path, styles

--- world_time/lib/pages/home.dart
+++ world_time/lib/pages/home.dart
@@ -19,44 +19,59 @@ class _HomeState extends State<Home> {
     Map data = jsonDecode(jsonEncode(parameters));
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: <Widget>[
-          OutlinedButton(
-            onPressed: () {
-              print('you clicked me');
-              Navigator.pushNamed(context, '/location');
-            },
-            child: Row(
-              children: const <Widget>[
-                Icon(Icons.edit_location),
-                Text('Edit Location'),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20.0),
-          Image.asset('assets/${data['flag']}.png'),
-          const SizedBox(height: 20.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                data['location'],
-                style: const TextStyle(
-                  fontSize: 28.0,
-                  letterSpacing: 2.0,
+        child: Column(
+          children: [
+            Container(
+              color: Colors.blue[900],
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue[900],
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Icon(Icons.edit_location),
+                    Text('Edit Location'),
+                  ],
                 ),
               ),
-            ],
-          ),
-          const SizedBox(height: 20.0),
-          Text(
-            data['time'],
-            style: const TextStyle(
-              fontSize: 66.0,
             ),
-          ),
-        ],
-      )),
+            const Spacer(
+              flex: 1,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 20.0),
+                Image.asset('assets/images/flags/${data['flag']}',
+                    height: 64.0),
+                const SizedBox(height: 20.0),
+                Text(
+                  data['location'],
+                  style: const TextStyle(
+                    fontSize: 28.0,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  data['time'],
+                  style: const TextStyle(
+                    fontSize: 66.0,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(flex: 2),
+          ],
+        ),
+      ),
     );
   }
 }
