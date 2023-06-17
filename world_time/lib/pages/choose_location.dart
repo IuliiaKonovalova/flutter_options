c6d94518255ec00c485d3051ec37baed7af486f2
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-06-17 12:45:21 +0700

    Add choose location functionality

--- world_time/lib/pages/choose_location.dart
+++ world_time/lib/pages/choose_location.dart
@@ -1,45 +1,75 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:world_time/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
-  const ChooseLocation({super.key});
-
   @override
   _ChooseLocationState createState() => _ChooseLocationState();
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
+  List<WorldTime> locations = [
+    WorldTime(
+      urlTail: 'Europe/London',
+      location: 'London',
+      flag: 'uk.png',
+      isDayTime: true,
+      time: '0',
+    ),
+    // WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    // WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    // WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    // WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    // WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    // WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    // WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTimeData();
+    Navigator.pop(context, {
+      'location': instance.location,
+      // 'time': instance.time,
+      'flag': instance.flag,
+      // 'isDaytime': instance.isDayTime,
+    });
+  }
 
   @override
   void initState() {
     super.initState();
-    print('initState function ran');
-    // getTimeData();
   }
 
   @override
   Widget build(BuildContext context) {
-    print('build function ran');
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: const Text('Choose a location'),
         backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
         centerTitle: true,
         elevation: 0,
       ),
-      body: ElevatedButton(
-        onPressed: () {
-          setState(() {
-            counter += 1;
-            print(counter);
-          });
-        },
-        child: Text('counter is $counter'),
-      ),
+      body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    updateTime(index);
+                  },
+                  title: Text(locations[index].location),
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        AssetImage('assets/${locations[index].flag}'),
+                  ),
+                ),
+              ),
+            );
+          }),
     );
   }
 }
