00ee6867d5f3e78055ad730b74c6d95e86058f3a
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-06-17 11:38:18 +0700

    Add world_time daytime control

--- world_time/lib/services/world_time.dart
+++ world_time/lib/services/world_time.dart
@@ -3,27 +3,32 @@ import 'package:http/http.dart';
 import 'package:intl/intl.dart';
 
 class WorldTime {
-  String location; // location name for the UI
+  String location;
   String time;
   String flag;
   String urlTail;
+  bool isDayTime;
 
   WorldTime({
     required this.location,
     required this.flag,
     required this.urlTail,
     required this.time,
+    required this.isDayTime,
   });
 
   Future<void> getTimeData() async {
     try {
       Response response = await get(Uri.parse(
           "https://timeapi.io/api/Time/current/zone?timeZone=$urlTail"));
+
       Map data = jsonDecode(response.body);
 
       String dateTime = data['dateTime'];
 
       DateTime now = DateTime.parse(dateTime);
+
+      isDayTime = now.hour > 6 && now.hour < 20 ? true : false;
 
       time = DateFormat.jm().format(now);
     } catch (e) {
@@ -33,9 +38,10 @@ class WorldTime {
   }
 }
 
-WorldTime instance = WorldTime(
-  location: 'Berlin',
-  flag: 'germany.png',
-  urlTail: 'Europe/Berlin',
-  time: '',
-);
+// WorldTime instance = WorldTime(
+//   location: 'Berlin',
+//   flag: 'germany.png',
+//   urlTail: 'Europe/Berlin',
+//   time: '',
+//   isDayTime: true,
+// );
